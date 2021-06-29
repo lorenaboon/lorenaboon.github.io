58c84d4b01eedb20d0eb87a999044115c9ccd2a7
lorenaboon <lorenaboon@student.rocmondriaan.nl>
2021-06-29 23:47:02 +0200

    bke aangepast op nieuwe manier nog niet werkend

--- js/bke.js
+++ js/bke.js
@@ -1,145 +1,111 @@
+import Player from "./Player.js";
 
-// Declaratie globale variabelen. ****boter kaas en eieren game****//
+const players = [ ];
+let currentPlayer = 0;
+const fields = document.querySelectorAll('.grid-item');
+const resetButton = document.querySelector(".reset-btn");
+const playerOne = new Player("Player 1", "X")
+const playerTwo = new Player("Player 2", "O")
 
-let players = []; // De elementen van de headers met namen.
-let cells = ["", "", "", "", "", "", "", "", ""]; // Alle cellen die later worden geselecteerd.
-let clickedCells = []; // Alle cellen waarop geklikt is.
-let currentPlayer;
-let playerNames = [];
+players.push(playerOne)
+players.push(playerTwo)
 
 
-let allCombs = [ // Alle mogelijke combinaties.
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-window.onload = function(){ // Als de pagina volledig is geladen zet dan deze variabelen.
-
-    cells = document.getElementsByTagName('td');
-    players = [document.getElementById("speler1"), document.getElementById("speler2")]; // De headers met namen van de spelers
-
-    clickedCells = [0, 0, 0, 0, 0, 0, 0, 0, 0]; //Alle kaarten in het spel, op volgorder van index.
-
-    document.getElementById('gameButton').addEventListener('click', startGame);
-};
-
-function startGame(){ // Nadat er geklikt is op de startknop
-    document.getElementById('gameButton').removeEventListener('click', startGame);
-    document.getElementById('gameButton').innerHTML = 'Reset';
-    document.getElementById('gameButton').addEventListener('click', resetEverything);
-    playerNames[0] = prompt('Naam speler O');
-    playerNames[1] = prompt('Naam speler X');
-
-    players[0].innerHTML = playerNames[0] + ' = O (Speler 1)';
-    players[1].innerHTML = playerNames[1] + ' = X (Speler 2';
-
-    currentPlayer = Math.floor(Math.random() * 2) + 1; // 1 of 2
-    document.getElementById('speler'+currentPlayer).style.color = 'plum';
-    enableClicking();
+for (let i = 0; i < fields.length; i++) {
+    const field = fields[i];
+    field.addEventListener("click", function() {
+        addSymbolToField(field);
+    });
 }
 
-function clickOnCell(event){
-	document.getElementById(event.target.id).removeEventListener('click', clickOnCell); // Zorgt ervoor dat je niet op een cell kan klikken waarop al geklikt is.
-    if(currentPlayer === 1){
-        cells[event.target.id].innerHTML = 'O';
-        document.getElementById('speler1').style.color = 'black';
-        document.getElementById('speler2').style.color = 'plum';
-        checkEnd();
-        currentPlayer = 2;
-    }else if(currentPlayer === 2){
-        cells[event.target.id].innerHTML = 'X';
-        document.getElementById('speler1').style.color = 'plum';
-        document.getElementById('speler2').style.color = 'black';
-        checkEnd();
-        currentPlayer = 1;
+resetButton.addEventListener("click", resetGame);
+
+// Symbool word toegevoegd
+function addSymbolToField(field) {
+    const fieldContent = field.textContent;
+    if (fieldContent === 'X' || fieldContent === 'O') {
+        alert('This field can not be used');
     }
-}
-
-function reset(){ // Reset ronde-dingen zoals het spelbord en het klikken.
-    currentPlayer = Math.floor(Math.random() * 2) + 1; // 1 of 2
-    clickedCells = [];
-    clearCells();
-    setTimeout(function(){ // Door een delay van de Math.floor functie kan de kleur eerder gezet worden en zijn de spelers verkeerd.
-        document.getElementById('speler1').style.color = 'black';
-        document.getElementById('speler2').style.color = 'black';
-        document.getElementById('speler'+currentPlayer).style.color = 'plum';
-    }, 1000);
-	enableClicking();
-}
-
-function resetEverything(){ // Herlaad de pagina wat de makkelijkste manier is om alles te resetten.
-	location.reload();
-}
-
-function enableClicking(){ // Zorgt dat je overal op kan klikken.
-    for(let i = 0; i < cells.length; i++){
-        document.getElementById(i).addEventListener('click', clickOnCell);
-    }
-}
-
-function clearCells(){ // Haal alle cells leeg van X of O.
-	for(let i = 0; i < cells.length; i++){
-		document.getElementById(i).innerHTML = "";
-	}
-}
-
-function checkEnd(){ // Kijkt elke zet of het spel eindigt
-    for(let i = 0; i < cells.length; i++){
-        clickedCells[i] = cells[i].innerHTML; // Array met alle gelikte cellen om het iets leesbaarder te maken in de volgende loop.
-    }
-
-    for(let a = 0; a < allCombs.length; a++) { // a was iets leesbaarder omdat het eerder samen zat.
-
-        /*
-        * Voorbeeld:
-        * Index 0 is ['0', '1', '2']
-        * Als 'geklikteCells' a (met index 0) en uit die twee-dimensionale array 0 x is, 1 x is en 2 x is is het een match.
-        * Dit blijft het doen met elke combinatie uit de array 'allCombs' om te kijken of er een match is als dat nog niet zo is.
-        */
-
-        if (clickedCells[allCombs[a][0]] === "X" && clickedCells[allCombs[a][1]] === "X" && clickedCells[allCombs[a][2]] === "X") {
-            alert('Winnaar is ' + playerNames[1] + ' - X');
-            addPoints('X');
-            break;
-        } else if (clickedCells[allCombs[a][0]] === "O" && clickedCells[allCombs[a][1]] === "O" && clickedCells[allCombs[a][2]] === "O") {
-
-            winner = playerNames[1];
-            alert('Winnaar is ' + playerNames[0] + ' - O');
-            addPoints('O');
-            break;
+    else{
+        if (currentPlayer == 0){
+            field.textContent = "X";
+            currentPlayer = 1;
+        }
+        else{
+            field.textContent = "O";
+            currentPlayer = 0;
         }
     }
-    for(let b = 0; b < allCombs.length; b++){
-        if(!clickedCells.includes("")){ // Als de array 'clickedCells' geen spatie bevat betekent het dat alles geklikt is en dat er geen match is, dus gelijkspel.
-			alert('Gelijk!');
-			gameEnd(); // Voegt geen punten toe en eindigt direct
-			break;
-		}
+    checkWinner()
+
+}
+
+// Alle winlijnen checken
+function checkWinner() {
+    const winline1 = [fields[0].textContent, fields[1].textContent, fields[2].textContent];
+    const winline2 = [fields[3].textContent, fields[4].textContent, fields[5].textContent];
+    const winline3 = [fields[6].textContent, fields[7].textContent, fields[8].textContent];
+    const winline4 = [fields[0].textContent, fields[3].textContent, fields[6].textContent];
+    const winline5 = [fields[1].textContent, fields[4].textContent, fields[7].textContent];
+    const winline6 = [fields[2].textContent, fields[5].textContent, fields[8].textContent];
+    const winline7 = [fields[0].textContent, fields[4].textContent, fields[8].textContent];
+    const winline8 = [fields[2].textContent, fields[4].textContent, fields[6].textContent];
+    if (winline1[0] == "X" && winline1[1] == "X" && winline1[2] == "X") {
+            alert("Player 1 heeft gewonnen!");
     }
-    // 012, 345, 678, 036, 147, 258, 047, 246 - Alle mogelijke combinaties
+    if (winline1[0] == "O" && winline1[1] == "O" && winline1[2] == "O") {
+            alert("Player 2 heeft gewonnen!");
+    }
+    if (winline2[0] == "X" && winline2[1] == "X" && winline2[2] == "X") {
+            alert("Player 1 heeft gewonnen!");
+    }
+    if (winline2[0] == "O" && winline2[1] == "O" && winline2[2] == "O") {
+            alert("Player 2 heeft gewonnen!");
+    }
+    if (winline3[0] == "X" && winline3[1] == "X" && winline3[2] == "X") {
+            alert("Player 1 heeft gewonnen!");
+    }
+    if (winline3[0] == "O" && winline3[1] == "O" && winline3[2] == "O") {
+            alert("Player 2 heeft gewonnen!");
+    }
+    if (winline4[0] == "X" && winline4[1] == "X" && winline4[2] == "X") {
+            alert("Player 1 heeft gewonnen!");
+    }
+    if (winline4[0] == "O" && winline4[1] == "O" && winline4[2] == "O") {
+            alert("Player 2 heeft gewonnen!");
+    }
+    if (winline5[0] == "X" && winline5[1] == "X" && winline5[2] == "X") {
+            alert("Player 1 heeft gewonnen!");
+    }
+    if (winline5[0] == "O" && winline5[1] == "O" && winline5[2] == "O") {
+            alert("Player 2 heeft gewonnen!");
+    }
+    if (winline6[0] == "X" && winline6[1] == "X" && winline6[2] == "X") {
+            alert("Player 1 heeft gewonnen!");
+    }
+    if (winline6[0] == "O" && winline6[1] == "O" && winline6[2] == "O") {
+            alert("Player 2 heeft gewonnen!");
+    }
+    if (winline7[0] == "X" && winline7[1] == "X" && winline7[2] == "X") {
+            alert("Player 1 heeft gewonnen!");
+    }
+    if (winline7[0] == "O" && winline7[1] == "O" && winline7[2] == "O") {
+            alert("Player 2 heeft gewonnen!");
+    }
+    if (winline8[0] == "X" && winline8[1] == "X" && winline8[2] == "X") {
+            alert("Player 1 heeft gewonnen!");
+    }
+    if (winline8[0] == "O" && winline8[1] == "O" && winline8[2] == "O") {
+            alert("Player 2 heeft gewonnen!");
+    }
 }
 
-function addPoints(winner){ // Voegt punten toe in de span onder de naam van de speler.
-	if(winner === 'O'){
-		document.getElementById("ps1").innerHTML++;
-	}else if(winner === 'X'){
-		document.getElementById("ps2").innerHTML++;
-	}
-	gameEnd();
+function resetGame() {
+    currentPlayer = 0;
+    for (let i = 0; i < fields.length; i++) {
+        const field = fields[i];
+        field.textContent = "-"
+    }
 }
 
-function gameEnd(){
-	let again = confirm('Nog een keer?');
-
-	if(again === true){
-		reset() // Reset het speelveld maar laat de punten staan.
-	}else{
-		resetEverything(); // Herlaad de pagina om opnieuw te beginnen.
-	}
-}
+console.log('File loaded');
